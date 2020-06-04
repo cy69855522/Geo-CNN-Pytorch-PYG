f3c664968109cf9a354eb79250cf39c962b1402a
Knife丶 <1272068154@qq.com>
2020-06-05 05:04:27 +0800

    Add files via upload

---
+++ geocnn.py
@@ -0,0 +1,311 @@
+# -*- coding: utf8 -*-
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import torch_geometric.transforms as T
+from torch_geometric.data import DataListLoader, DataLoader
+from torch_geometric.nn import fps, DataParallel
+
+import math
+import time
+import random
+import numpy as np
+from utils import *
+
+seed = 1
+random.seed(seed)
+np.random.seed(seed)
+torch.manual_seed(seed)
+torch.cuda.manual_seed(seed)
+torch.cuda.manual_seed_all(seed)
+
+
+# Configuration
+
+file_name = __file__.split('.')[0]
+batch_size = 2
+num_workers = 4
+cuda = torch.cuda.is_available()
+accumulation_steps = 1  # Gradient accumulation steps
+device_list = [0]  # Can be multiple, for example, [0, 1] use GPU0 and GPU1
+torch.cuda.set_device(device_list[0])
+device = torch.device('cuda' if cuda else 'cpu')
+base_lr = 0.001
+epoch = 500
+bacc = 0  # Used to record the best accuracy
+test_ratio = 0.2  # If bacc reaches lacc, we test every test_ratio epochs
+lacc = 0.92
+only_test = False
+
+# Load data
+
+pre_transform = UnitSphere()
+train_transform = T.Compose([
+    ShufflePoints(),
+    ChunkPoints(1000),
+    ScalePoints((2./3., 3./2.)),
+    MovePoints((-0.02, 0.02)),
+    #Jitter(),
+])
+test_transform = T.Compose([
+    ChunkPoints(1000),
+])
+
+train_dataset = ModelNet40_10000('data/ModelNet40_10000', True, train_transform, pre_transform)
+test_dataset = ModelNet40_10000('data/ModelNet40_10000', False, test_transform, pre_transform)
+
+DLF = DataListLoader if cuda else DataLoader
+train_loader = DLF(
+    train_dataset,
+    batch_size=batch_size,
+    shuffle=True,
+    drop_last=False,
+    pin_memory=True,
+    num_workers=num_workers
+)
+test_loader = DLF(
+    test_dataset,
+    batch_size=batch_size,
+    shuffle=False,
+    pin_memory=True,
+    num_workers=num_workers
+)
+
+
+# Define model
+
+class MLP_md(nn.Module):
+    def __init__(self, channels, norm='bn', resi=False, md=1, **kwargs):
+        super(MLP_md, self).__init__()
+        self.linears = nn.ModuleList([eval(f'nn.Conv{md}d')(i, o, 1) for i, o in zip(channels, channels[1:])])
+        self.norms = nn.ModuleList([Norm(norm, o, md=md, **kwargs) for o in channels[1:]])
+        self.actis = nn.ModuleList([nn.ReLU(inplace=True) for _ in channels[1:]])
+        self.resi = resi
+
+    def forward(self, x, feature_last=True): # [B, ..., C]
+        twoD = len(x.shape) == 2
+        if twoD:
+            feature_last = False
+            x = x.unsqueeze(-1)
+        if feature_last: x = x.transpose(1, -1) # [B, C, ...]
+        
+        for linear, norm, acti in zip(self.linears, self.norms, self.actis):
+            inp = x if self.resi else None
+            x = linear(x)
+            x = norm(x)
+            x = acti(x)
+            if inp is not None and x.shape == inp.shape:
+                x = x + inp
+        
+        if feature_last:
+            x = x.transpose(1, -1) # [B, ..., C]
+            x = x.contiguous()
+        if twoD: x = x.squeeze(-1)
+
+        return x
+
+class GeoConv(nn.Module):
+    def __init__(self, in_channels, hidden_channels, out_channels, norm='bn'):
+        super(GeoConv, self).__init__()
+        self.lin1 = nn.Linear(in_channels, out_channels)
+        self.lin2 = nn.Linear(hidden_channels, out_channels)
+        self.lins = nn.ModuleList([nn.Linear(in_channels, hidden_channels) for _ in range(6)])
+        self.norm1 = Norm(norm, hidden_channels, md=1)
+        self.acti1 = nn.ReLU(inplace=True)
+        self.norm2 = Norm(norm, out_channels, md=1)
+        self.acti2 = nn.ReLU(inplace=True)
+
+    def forward(self, x, p, B, n, id_euc):
+        # x[B*N, C] p[B*N, 3] sid/tid[B*n*k]
+        sid_euc, tid_euc = id_euc
+        k = int(len(sid_euc) / B / n)
+        dev = x.device
+
+        euc_i, euc_j = x[tid_euc], x[sid_euc] # [B*n*k, C]
+        edge = euc_j - euc_i
+        
+        p_diff = p[sid_euc] - p[tid_euc] # [B*n*k, 3]
+        p_dis = p_diff.norm(dim=-1, keepdim=True).clamp(min=1e-16) # [B*n*k, 1]
+        p_cos = (p_diff / p_dis).cos()**2 # [B*n*k, 3]
+        p_cos = p_cos.transpose(0, 1).reshape(-1, B, n, k, 1) # [3, B, n, k, 1]
+        bid = (p_diff > 0).long() # [B*n*k, 3]
+        bid += torch.tensor([0, 2, 4], device=dev, dtype=torch.long).view(1, 3)
+        edge = torch.stack([lin(edge) for lin in self.lins]) # [bases, B*n*k, C]
+        edge = torch.stack([edge[bid[:, i], range(B*n*k)] for i in range(3)]) # [3, B*n*k, C]
+        edge = edge.view(3, B, n, k, -1)
+        edge = edge * p_cos # [3, B, n, k, C]
+        edge = edge.sum(dim=0) # [B, n, k, C]
+        p_dis = p_dis.view(B, n, k, 1)
+        p_r = p_dis.max(dim=2, keepdim=True)[0] * 1.1 # [B, n, 1, 1]
+        p_d = (p_r - p_dis)**2 # [B, n, k, 1]
+        edge = edge * p_d / p_d.sum(dim=2, keepdim=True) # [B, n, k, C]
+        y = edge.sum(dim=2).transpose(1, -1) # [B, C, n]
+        y = self.acti1(self.norm1(y)).transpose(1, -1) # [B, n, C]
+        x = self.lin1(x[tid_euc[::k]]).view(B, n, -1) # [B, n, C]
+        y = x + self.lin2(y) # [B, n, C]
+        y = y.transpose(1, -1) # [B, C, n]
+        y = self.acti2(self.norm2(y))
+        y = y.transpose(1, -1) # [B, n, C]
+        y = y.flatten(0, 1) # [B*n, C]
+        
+        return y
+
+class PointPlus(nn.Module):  # PointNet++
+    def __init__(self, in_channels, out_channels, norm='bn', first_layer=False):
+        super(PointPlus, self).__init__()
+        self.first_layer = first_layer
+        self.fc1 = MLP_md([in_channels, out_channels], norm=norm, md=2)
+
+    def forward(self, x, B, n, id_euc):
+        # x[B*N, C] sid/tid[B*n*k]
+        sid_euc, tid_euc = id_euc
+        k = int(sid_euc.size(0) / B / n)
+
+        if self.first_layer:
+            x, norm = x[:, :3], x[:, 3:]
+            x_i, x_j = x[tid_euc], x[sid_euc] # [B*n*k, C]
+            norm_j = norm[sid_euc] # [B*n*k, C]
+            edge = torch.cat([x_j - x_i, norm_j], dim=-1) # [B*n*k, C]
+        else:
+            x_i, x_j = x[sid_euc], x[tid_euc] # [B*n*k, C]
+            edge = x_j - x_i
+        edge = edge.view(B, n, k, -1) # [B, n, k, C]
+        edge = self.fc1(edge) # [B, n, k, C]
+        y = edge.max(2)[0] # [B, n, C]
+        y = y.view(B*n, -1) # [B*n, C]
+
+        return y
+
+class Net(nn.Module):
+    def __init__(self, out_channels, norm='bn'):
+        super(Net, self).__init__()
+
+        # branch1
+        self.pp1 = PointPlus(6, 64, norm, first_layer=True)
+        self.pp2 = PointPlus(64, 128, norm)
+        self.pp3 = PointPlus(128, 384, norm)
+
+        # branch2
+        self.lin1 = MLP_md([6, 64])
+        self.conv1 = GeoConv(64, 64, 128, norm=norm)
+        self.lin2 = MLP_md([128, 256])
+        self.conv2 = GeoConv(256, 64, 512, norm=norm)
+
+        # master
+        self.conv3 = GeoConv(896, 64, 768, norm=norm)
+        self.lin3 = MLP_md([768, 2048], norm=norm)
+        self.fc = nn.Sequential(
+            nn.Linear(2048, 512),
+            Norm(norm, 512, channels_per_group=128),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5),
+            nn.Linear(512, 256),
+            Norm(norm, 256, channels_per_group=128),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5),
+            nn.Linear(256, out_channels)
+        )
+
+    def forward(self, data):
+        pos, norm, batch = data.pos, data.norm, data.batch
+        dev = pos.device
+        B = batch.max().item() + 1
+        N = int(pos.size(0) / B)
+
+        x = torch.cat([pos, norm], dim=-1)
+
+        # branch1
+        id_euc = knn(pos.view(B, N, -1), 16)
+        x1 = self.pp1(x, B, N, id_euc)  # [B*N, C]
+        x2 = self.pp2(x1, B, N, id_euc)
+        x3 = self.pp3(x2, B, N, id_euc)
+
+        # branch2
+        x4 = self.lin1(x.view(B, N, -1)).view(B*N, -1)  # [B*N, C]
+        id_euc = sphg(pos, 0.15, batch=batch, max_num_neighbors=16)
+        x5 = self.conv1(x4, pos, B, N, id_euc)  # [B*N, C]
+
+        x6 = self.lin2(x5.view(B, N, -1)).view(B*N, -1)
+        id_euc = sphg(pos, 0.3, batch=batch, max_num_neighbors=16)
+        x7 = self.conv2(x6, pos, B, N, id_euc)  # [B*N, C]
+
+        # master
+        x8 = torch.cat([x3, x7], dim=-1)  # [B*N, C]
+        id_euc = sphg(pos, 0.6, batch=batch, max_num_neighbors=16)
+        x9 = self.conv3(x8, pos, B, N, id_euc)
+        x10 = self.lin3(x9.view(B, N, -1))
+        x = x10.max(1)[0]  # [B, C]
+        
+        return self.fc(x)
+
+
+# Train and test
+
+model = Net(train_dataset.num_classes)
+model = model.to(device)
+model.load_state_dict(torch.load('weight.pth', map_location=f'cuda:{device_list[0]}'), strict=True)
+if cuda: model = DataParallel(model, device_ids=device_list)
+optimizer = torch.optim.Adam([{'params': model.parameters(), 'initial_lr': base_lr}], lr=base_lr, weight_decay=1e-4)
+# optimizer.load_state_dict(torch.load('geocnn_optimizer.pt', map_location=f'cuda:{device_list[0]}').state_dict())
+scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, epoch, eta_min=0.00001, last_epoch=-1)
+criterion = cal_loss
+
+def train(epoch):
+    model.train()
+    test_interval = int(test_ratio * len(train_loader))
+    total_loss = correct = num_data = 0
+    for i, data_list in enumerate(train_loader):
+        since = time.time()
+        y = torch.cat([data.y for data in data_list]) if cuda else data_list.y
+        y = y.to(device)
+        out = model(data_list)
+        loss = criterion(out, y)
+        loss /= accumulation_steps * len(device_list)
+        pred = out.max(dim=1)[1]
+        loss.backward()
+        if (i + 1) % accumulation_steps == 0:
+            optimizer.step()
+            optimizer.zero_grad()
+        total_loss += loss.item() * accumulation_steps
+        correct += pred.eq(y).sum().item()
+        num_data += len(data_list)
+
+        show = 100
+        if (i + 1) % show == 0:
+            print('{:.4f}s [{}/{}] Loss: {:.4f}, Train Accuracy: {:.4f}, LR: {:.5f}'.format(
+                time.time() - since, i + 1, len(train_loader), total_loss / show,
+                correct / num_data, scheduler.get_lr()[0]))
+            total_loss = correct = num_data = 0
+
+        if i % test_interval == 0 and bacc > lacc:
+            test(epoch)
+            model.train()
+
+def test(epoch=0):
+    model.eval()
+    global bacc
+    correct = 0
+    for data_list in test_loader:
+        with torch.no_grad():
+            out = model(data_list)
+        pred = out.max(dim=1)[1]
+        y = torch.cat([data.y for data in data_list]) if cuda else data_list.y
+        y = y.to(device)
+        correct += pred.eq(y).sum().item()
+
+    acc = correct / len(test_loader.dataset)
+    if bacc < acc:
+        bacc = acc
+        torch.save(model.module.state_dict(), f"{file_name}_epoch_{epoch}_{bacc}.pth")
+        torch.save(optimizer, f'{file_name}_optimizer.pt')
+
+    print('[Epoch: %d BestAcc: %.4f] Acc: %.4f' % (epoch, bacc, acc))
+    return acc
+
+for epoch in range(epoch):
+    if not only_test:
+        train(epoch)
+        scheduler.step(epoch)
+    test(epoch)
---
+++ utils.py
@@ -0,0 +1,248 @@
+# -*- coding: utf8 -*-
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from torch_geometric.data import InMemoryDataset, Data
+
+import math
+import random
+from tqdm import tqdm
+from itertools import repeat
+import os.path as osp
+import os
+import h5py
+
+class ShufflePoints(object):
+    def __call__(self, data):
+        idx = torch.randperm(data.pos.size(0))
+        data['pos'] = data.pos[idx]
+        if data.norm is not None:
+            data.norm = data.norm[idx]
+        return data
+
+class MovePoints(object):
+    def __init__(self, mrange=(-0.2, 0.2)):
+        self.low, self.high = mrange
+
+    def __call__(self, data):
+        data['pos'] += torch.ones(3, dtype=data.pos.dtype, device=data.pos.device).uniform_(self.low, self.high)
+        return data
+
+
+class ScalePoints(object):
+    def __init__(self, srange=(2./3, 3./2)):
+        self.low, self.high = srange
+
+    def __call__(self, data):
+        data['pos'] *= torch.ones(3, dtype=data.pos.dtype, device=data.pos.device).uniform_(self.low, self.high)
+        return data
+
+class Jitter(object):
+    def __init__(self, std=0.01, clip=0.05):
+        self.std, self.clip = std, clip
+
+    def __call__(self, data):
+        jittered_data = data.pos.new(data.pos.size(0), 3).normal_(
+            mean=0.0, std=self.std
+        ).clamp_(-self.clip, self.clip)
+        data['pos'] += jittered_data
+        
+        return data
+
+    def __repr__(self):
+        return '{}(std: {},  clip: {})'.format(self.__class__.__name__, self.std, self.clip)
+
+class ChunkPoints(object):
+    def __init__(self, num_points=1024, random_start=False):
+        self.N = num_points
+        self.random_start = random_start
+        
+    def __call__(self, data):
+        start = 0
+        if self.random_start:
+            start = random.randint(0, data.pos.size(0) - self.N)
+        data['pos'] = data.pos[start : start + self.N]
+        if data.norm is not None:
+            data.norm = data.norm[start : start + self.N]
+        return data
+
+class UnitSphere(object):
+    r"""Centers and normalizes node positions to an unit sphere.
+    """
+    def __call__(self, data):
+        data.pos -= data.pos.mean(dim=-2)
+        data.pos /= data.pos.norm(dim=-1).max()
+
+        return data
+
+    def __repr__(self):
+        return '{}()'.format(self.__class__.__name__)
+
+def Norm(name, c, channels_per_group=16, momentum=0.1, md=1):
+    if name == 'bn':
+        return eval(f'nn.BatchNorm{md}d')(c, momentum=momentum)
+    elif name == 'gn':
+        num_group = c // channels_per_group
+        if num_group * channels_per_group != c:
+            num_group = 1
+        return nn.GroupNorm(num_group, c)
+
+class ModelNet40_10000(InMemoryDataset):
+
+    def __init__(self, root, train=True, transform=None, pre_transform=None, pre_filter=None):
+        self.name = '40'
+        super(ModelNet40_10000, self).__init__(root, transform, pre_transform, pre_filter)
+        path = self.processed_paths[0] if train else self.processed_paths[1]
+        self.data, self.slices = torch.load(path)
+
+    @property
+    def raw_file_names(self):
+        return [
+            'bathtub', 'bed', 'chair', 'desk', 'dresser', 'monitor',
+            'night_stand', 'sofa', 'table', 'toilet'
+        ]
+
+    @property
+    def processed_file_names(self):
+        return ['training.pt', 'test.pt']
+
+    def process(self):
+        torch.save(self.process_set('train'), self.processed_paths[0])
+        torch.save(self.process_set('test'), self.processed_paths[1])
+
+    def process_set(self, dataset):
+        f = osp.join(self.raw_dir, f'modelnet{self.name}_shape_names.txt')
+        with open(f, 'r') as f:
+            categories = f.read().split('\n')[:-1]
+            cate_id = {cate : i for i, cate in enumerate(categories)}
+            
+        f = osp.join(self.raw_dir, f'modelnet{self.name}_{dataset}.txt')
+        with open(f, 'r') as f:
+            file_list = f.read().split('\n')[:-1]
+        
+        data_list = []
+        with tqdm(file_list) as t:
+            for file_name in t:
+                category = '_'.join(file_name.split('_')[:-1])
+                f = osp.join(self.raw_dir, category, f'{file_name}.txt')
+                data = read_txt_array(f, sep=',')
+                data = Data(pos=data[:, :3], norm=data[:, 3:])
+                data.y = torch.tensor([cate_id[category]])
+                data_list.append(data)
+
+        if self.pre_filter is not None:
+            data_list = [d for d in data_list if self.pre_filter(d)]
+
+        if self.pre_transform is not None:
+            data_list = [self.pre_transform(d) for d in data_list]
+
+        return self.collate(data_list)
+
+    def __repr__(self):
+        return '{}{}({})'.format(self.__class__.__name__, self.name, len(self))
+    
+    def get(self, idx):
+        data = self.data.__class__()
+
+        if hasattr(self.data, '__num_nodes__'):
+            data.num_nodes = self.data.__num_nodes__[idx]
+
+        for key in self.data.keys:
+            item, slices = self.data[key], self.slices[key]
+            if torch.is_tensor(item):
+                s = list(repeat(slice(None), item.dim()))
+                s[self.data.__cat_dim__(key,
+                                        item)] = slice(slices[idx],
+                                                       slices[idx + 1])
+            else:
+                s = slice(slices[idx], slices[idx + 1])
+            data[key] = item[s]
+            
+        data.pos = data.pos.clone()
+        if data.norm is not None: data.norm = data.norm.clone()
+        data['path_id'] = idx
+        
+        return data
+
+def cal_loss(pred, gold, smoothing=True):
+    ''' Calculate cross entropy loss, apply label smoothing if needed. '''
+    gold = gold.contiguous().view(-1)
+
+    if smoothing:
+        eps = 0.2
+        n_class = pred.size(1)
+
+        one_hot = torch.zeros_like(pred).scatter(1, gold.view(-1, 1), 1)
+        one_hot = one_hot * (1 - eps) + (1 - one_hot) * eps / (n_class - 1)
+        log_prb = F.log_softmax(pred, dim=1)
+
+        loss = -(one_hot * log_prb).sum(dim=1).mean()
+    else:
+        loss = F.cross_entropy(pred, gold, reduction='mean')
+
+    return loss
+
+def knn(x, k, d=1, f=None): # x[B, N, C] f[B*n]
+    B, N, _ = x.size()
+    n = N if f is None else int(f.size(0) / B)
+    dev = x.device
+    inner = -2 * torch.matmul(x, x.transpose(2, 1)) # [B, N, N]
+    xx = torch.sum(x**2, dim=-1, keepdim=True) # [B, N, 1]
+    dis = -xx.transpose(2, 1) - inner - xx # [B, N, N]
+    if f is not None: dis = dis.view(B*N, N)[f].view(B, n, N)
+    sid = dis.topk(k=k*d-d+1, dim=-1)[1][..., ::d] # (B, n, k)
+    sid += torch.arange(B, device=dev).view(B, 1, 1) * N
+    sid = sid.reshape(-1) # [B*n*k]
+    tid = torch.arange(B * N, device=dev) if f is None else f # [B*n]
+    tid = tid.view(-1, 1).repeat(1, k).view(-1) # [B*n*k]
+    return sid, tid # [B*n*k]
+
+def sphg(pos, r, batch=None, flow='source_to_target', max_num_neighbors=48, fpsi=None, resetFpsi=False, random_replace=True):
+    # If the "pos" contains points of the same position, the self-loop may be generated even if the loop is set to False
+    # Make sure "batch" is ascending
+    assert flow in ['source_to_target', 'target_to_source']
+
+    B = batch[-1].item() + 1
+    N = int(len(pos) / B)
+    n = N if fpsi is None else int(len(fpsi) / B)
+    C = pos.size(-1)
+    k = max_num_neighbors
+    dev = pos.device
+    
+    with torch.no_grad():
+      pos_i = pos.view(B, N, C) if fpsi is None else pos[fpsi].view(B, n, C)
+      pos_j = pos.view(B, N, C)
+      dis = pos_i.unsqueeze(-2).expand(B, n, N, C) - pos_j.unsqueeze(-2).transpose(1, 2)
+      dis = dis.norm(dim=-1) # [B, n, N]
+      max_valid_neighbors = max(k, (dis <= r).sum(dim=-1).max())
+      dis, sid = dis.topk(max_valid_neighbors, largest=False) # [B, n, max_valid_neighbors]
+      sid += torch.arange(B, device=dev, dtype=sid.dtype).view(B, 1, 1) * N
+      invalid_mask = dis > r # [B, n, max_valid_neighbors]
+      # For those have too many valid neighbors, randomly shuffle and choose without repetition
+      shuffle_order = torch.rand(B, n, max_valid_neighbors, device=dev)
+      shuffle_order[invalid_mask] = -1
+      _, shuffle_order = shuffle_order.topk(k, largest=True)
+      sid = sid.gather(-1, shuffle_order)[..., :k] # [B, n, k]
+      # Invalid neighbors are clustered at the end, so we can intercept the mask directly
+      invalid_mask = invalid_mask[..., :k] # [B, n, k]
+      # For those have less valid neighbors, randomly replace all invalid neighbors
+      if random_replace:
+          replacement = torch.rand(B, n, k, device=dev) * (k - invalid_mask.float().sum(dim=-1, keepdim=True))
+          replacement.floor_()
+          replacement.clamp_(max=k-1)
+          replacement = sid.gather(-1, replacement.long())
+          sid[invalid_mask] = replacement[invalid_mask]
+      else:
+          sid[invalid_mask] = sid[..., 0:1].expand(B, n, k)[invalid_mask]
+      sid = sid.view(-1)
+      if fpsi is None or resetFpsi: fpsi = torch.arange(B * n, device=dev)
+      tid = fpsi.view(-1, 1).repeat(1, k).view(-1)
+    
+    return (sid, tid) if flow == 'source_to_target' else (tid, sid)
+    
+
+if __name__ == "__main__":
+    train_dataset = ModelNet40('/mnt/md0/data/ModelNet40_10000', '40')
+    draw(train_dataset[0].pos)
